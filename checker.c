5f673210a7dcbaeaeb2bae4cb0cbca114cba1dff
Sekaii22 <shawn.ng22@gmail.com>
2025-04-20 06:25:06 +0800

    Add calculateDirHash function

--- checker.c
+++ checker.c
@@ -2,12 +2,17 @@
 #include <string.h>
 #include <openssl/evp.h>
 #include <openssl/bio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
 #define BUFFER_LEN 1024
 #define MAX_PATHS 1000
 #define MD5_HEX_LEN 32 
 #define MONITOR_FILE_PATH "monitor.txt"
 #define HASH_STORE_FILE_PATH "hashes.txt"
 #define LOG_FILE_PATH "checker.log"
+#define MSG_LEN 300
 
 /* 
     BUILD COMMAND: gcc -fsanitize=address -o checker checker.c -lssl -lcrypto
@@ -31,7 +36,7 @@ void printFileFailRead(char *filePath) {
     printf("Abort execution...\n");
 
     // print to log
-    char msg[300];
+    char msg[MSG_LEN];
     sprintf(msg, "Fail to read: %s", filePath);
     logger(msg);
     logger("Abort execution...");
@@ -105,13 +110,222 @@ int calculateHash(char *filePath, char *output) {
         // len is the address offset
         len += sprintf(hashBuffer + len, "%02x", digestOutput[i]);
     }
+    strcpy(output, hashBuffer);
 
     // clean up all the resources
     EVP_MD_free(md5P);
     EVP_MD_CTX_free(ctxP);
     fclose(file);
 
-    strcpy(output, hashBuffer);
+    return 0;
+}
+
+/*
+    Check if path given exists and is a file.
+    Return 1 if true, otherwise 0.
+*/
+int isFile(const char *path) {
+    // stat stores info about a path
+    struct stat stats;
+    int isPathExist;
+
+    // fill stats with data about the path given
+    // stat() returns 0 if successful
+    isPathExist = stat(path, &stats);
+
+    // S_ISREG returns non-zero if path is a regular file
+    if (isPathExist == 0 && S_ISREG(stats.st_mode)) {
+        return 1;
+    }
+
+    return 0;
+}
+
+/*
+    Check if path given exists and is a directory.
+    Return 1 if true, otherwise 0.
+*/
+int isDir(const char *path) {
+    struct stat stats;
+    int isPathExist;
+
+    isPathExist = stat(path, &stats);
+
+    // S_ISDIR returns non-zero if path is a dir
+    if (isPathExist == 0 && S_ISDIR(stats.st_mode)) {
+        return 1;
+    }
+
+    return 0;
+}
+
+/*
+    comparison function for qsort, sort strings in ascending order.
+    *a and *b are void pointers to values.
+*/
+int strCompare(const void *a, const void *b) {
+    // void pointers need to first be casted
+    // into a pointer to char array and then
+    // dereferenced to get the value.
+    return strcmp(*(const char **)a, *(const char **)b);
+}
+
+int calculateDirHash(char *dirPath, char *finalOutput) {
+    struct dirent *dirEntry;
+    DIR *dirP = opendir(dirPath);
+    if (dirP == NULL){
+        printf("%s cannot be opened\n", dirPath);
+
+        char msg[300];
+        sprintf(msg, "%s cannot be opened", dirPath);
+        logger(msg);
+
+        return -1;
+    }
+
+    int fileCount = 0;
+    int dirCount = 0;
+
+    // get file and dir count
+    while (dirEntry = readdir(dirP)) {
+        if (strcmp(dirEntry->d_name, ".") != 0 && strcmp(dirEntry->d_name, "..") != 0) {
+            char newPath[300] = "";
+            sprintf(newPath, "%s/%s", dirPath, dirEntry->d_name);
+            //printf("Path detected: %s\n", newPath);
+
+            if (isFile(newPath)) 
+                fileCount++;
+            else if (isDir(newPath))
+                dirCount++;
+        }
+    }
+
+    // printf("File Count: %d\n", fileCount);
+    // printf("Dir Count: %d\n", dirCount);
+    char *filePathArr[fileCount];
+    char *dirPathArr[dirCount];
+    int fileArrIndex = 0;
+    int dirArrIndex = 0;
+    
+    rewinddir(dirP);
+
+    // store file and dir path into respective arrays
+    while (dirEntry = readdir(dirP)) {
+        // ignore . and .. but allow hidden files
+        if (strcmp(dirEntry->d_name, ".") != 0 && strcmp(dirEntry->d_name, "..") != 0) {
+            char newPath[300] = "";
+            sprintf(newPath, "%s/%s", dirPath, dirEntry->d_name);
+
+            if (isFile(newPath)) {
+                // store in file array
+                filePathArr[fileArrIndex] = malloc(strlen(newPath) + 1);
+                strcpy(filePathArr[fileArrIndex], newPath);
+
+                fileArrIndex++;
+            }
+            else if (isDir(newPath)) {
+                // store in dir array
+                dirPathArr[dirArrIndex] = malloc(strlen(newPath) + 1);
+                strcpy(dirPathArr[dirArrIndex], newPath);
+
+                dirArrIndex++;
+            }
+        }
+    }
+
+    // sort file and dir array
+    qsort(filePathArr, fileCount, sizeof(const char *), strCompare);
+    qsort(dirPathArr, dirCount, sizeof(const char *), strCompare);
+
+    // set up hashing
+    EVP_MD_CTX *ctxP = NULL;
+    EVP_MD *md5P = NULL;
+    unsigned char digestOutput[16];
+
+    // create new context, fetch md5 algo, initialize digest operation
+    ctxP = EVP_MD_CTX_new();
+    if (!ctxP) {
+        printf("Digest context is null\n");
+        logger("Digest context is null");
+        return -1;
+    }
+
+    md5P = EVP_MD_fetch(NULL, "MD5", NULL);
+    if (md5P == NULL) {
+        printf("Fetching failed\n");
+        logger("Fetching failed");
+        return -1;
+    }
+
+    if (!EVP_DigestInit_ex(ctxP, md5P, NULL)) {
+        printf("Error with initializing digest\n");
+        logger("Error with initializing digest");
+        return -1;
+    }
+
+    // for file in file array
+    //      call calculateHash(file, output)
+    //      call digestupdate with output
+    for (int i = 0; i < fileCount; i++) {
+        char intermediateHash[MD5_HEX_LEN + 1];
+
+        // calculate hash for current file and put into intermediateHash
+        calculateHash(filePathArr[i], intermediateHash);
+        
+        // use intermediate file hash for calculating final hash
+        if (!EVP_DigestUpdate(ctxP, intermediateHash, strlen(intermediateHash))) {
+            printf("Error with digest update\n");
+            logger("Error with digest update");
+            return -1;
+        }
+    }
+
+    // for dir in dir array
+    //      call calculateDirHash(dir, output)
+    //      call digestupdate with output
+    for (int i = 0; i < dirCount; i++) {
+        char intermediateHash[MD5_HEX_LEN + 1];
+
+        // calculate hash for dir and put into intermediateHash
+        calculateDirHash(dirPathArr[i], intermediateHash);
+        
+        // use intermediate dir hash for calculating final hash
+        if (!EVP_DigestUpdate(ctxP, intermediateHash, strlen(intermediateHash))) {
+            printf("Error with digest update\n");
+            logger("Error with digest update");
+            return -1;
+        }
+    }
+    
+    // calculate final output digest
+    if (!EVP_DigestFinal_ex(ctxP, digestOutput, NULL)) {
+        printf("Error with calculating final digest\n");
+        logger("Error with calculating final digest");
+        return -1;
+    }
+
+    //store hash hex string as literal in hashBuffer and copy into finalOutput
+    char hashBuffer[MD5_HEX_LEN + 1];
+    int len = 0;
+
+    for (int i = 0; i < sizeof(digestOutput); i++) {
+        len += sprintf(hashBuffer + len, "%02x", digestOutput[i]);
+    }
+    strcpy(finalOutput, hashBuffer);
+
+    // clean up all the resources
+    for (int i = 0; i < fileCount; i++) {
+        free(filePathArr[i]);
+    }
+
+    for (int i = 0; i < dirCount; i++) {
+        free(dirPathArr[i]);
+    }
+
+    EVP_MD_free(md5P);
+    EVP_MD_CTX_free(ctxP);
+    closedir(dirP);
+
     return 0;
 }
 
@@ -175,7 +389,7 @@ int init() {
         fprintf(hashStoreFileP, "%s %s\n", hash, monitoredPaths[i]);
 
         // log results
-        char msg[300];
+        char msg[MSG_LEN];
         sprintf(msg, "%s %s", hash, monitoredPaths[i]);
         logger(msg);
     }
@@ -224,7 +438,7 @@ int check() {
             printf("Current hash:\t %s\n\n", newHash);
 
             // log diff
-            char msg[300];
+            char msg[MSG_LEN];
             sprintf(msg, "%s has changed!", filePath);
             logger(msg);
             sprintf(msg, "Previous hash:\t %s", oldHash);
@@ -258,6 +472,10 @@ int main(int argc, char *argv[]) {
     
     // no args given
     if (argc <= 1) {
+        char temp[MD5_HEX_LEN + 1];
+        calculateDirHash("files_to_monitor", temp);
+        printf("Final hash: %s\n", temp);
+
         printf("No arguments given. Use -h or --help for more information.\n");
         logger("No arguments given.");
         return 0;
